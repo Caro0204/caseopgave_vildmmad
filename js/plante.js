c59b5ebd2a6724d1efaeba082bbc0a593bca6ede
Melbye <simo80k3@stud.kea.dk>
2023-09-14 15:03:18 +0200

    plante

--- js/plante.js
+++ js/plante.js
@@ -4,7 +4,7 @@ const url = "https://oyneqjqpxkustcsgrvut.supabase.co";
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95bmVxanFweGt1c3Rjc2dydnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NjYyMjksImV4cCI6MjAwOTQ0MjIyOX0.LTjODVfOec0K5fQg8jjs6TXgJRyaP8Ulyzr1-NTw5X8";
 
-fetch(url + "/rest/v1/vildmad", {
+fetch(url + "/rest/v1/vildmad?id=eq.5" , {
   method: "GET",
   headers: {
     apikey: key,
@@ -21,13 +21,15 @@ function showData(items) {
 
 
 function showItem(item) {
-  const template = document.querySelector("#plante").content.cloneNode(true);
-  document.querySelector("p.Sankested").textContent = item.Sankested;
-  document.querySelector("p.Saeson").textContent = item.Saeson;
-  document.querySelector("p.Beskrivelse").textContent = item.Beskrivelse;
-  document.querySelector("p.Sankning").textContent = item.Sankning;
-  document.querySelector("p.Ingredienser").textContent = item.Ingredienser;
-  document.querySelector("p.Fremgangsmåde").textContent = item.Fremgangsmåde;
 
-  document.querySelector(".margin_plante").appendChild(copy);
+  const template = document.querySelector("#plante").content.cloneNode(true);
+  template.querySelector(".title").textContent = item.title;
+  template.querySelector(".Sankested").textContent = item.Sankested;
+  template.querySelector(".Saeson").textContent = item.Saeson;
+  template.querySelector(".Beskrivelse").textContent = item.Beskrivelse;
+  template.querySelector(".Sankning").textContent = item.Sankning;
+  template.querySelector(".Ingredienser").textContent = item.Ingredienser;
+  template.querySelector(".Fremgangsmåde").textContent = item.Fremgangsmetode;
+
+  document.querySelector("#margin_plante").appendChild(template);
 }
--- js/planteliste.js
+++ js/planteliste.js
@@ -2,7 +2,7 @@ const url = "https://oyneqjqpxkustcsgrvut.supabase.co";
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95bmVxanFweGt1c3Rjc2dydnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NjYyMjksImV4cCI6MjAwOTQ0MjIyOX0.LTjODVfOec0K5fQg8jjs6TXgJRyaP8Ulyzr1-NTw5X8";
 
-fetch(url + "/rest/v1/vildmad", {
+fetch(url + "/rest/v1/vildmad?id", {
   method: "GET",
   headers: {
     apikey: key,
