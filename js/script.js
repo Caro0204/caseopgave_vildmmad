a37cef433f60018baca2fb513e356c15365618b3
Caroline <cave0001@stud.ked.dk>
2023-09-14 08:43:32 +0200

    java

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-fetch("https://oyneqjqpxkustcsgrvut.supabase.co/rest/v1/vildmad", {
+const url = "https://oyneqjqpxkustcsgrvut.supabase.co";
+
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95bmVxanFweGt1c3Rjc2dydnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NjYyMjksImV4cCI6MjAwOTQ0MjIyOX0.LTjODVfOec0K5fQg8jjs6TXgJRyaP8Ulyzr1-NTw5X8";
+
+fetch(url + "/rest/v1/vildmad", {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95bmVxanFweGt1c3Rjc2dydnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NjYyMjksImV4cCI6MjAwOTQ0MjIyOX0.LTjODVfOec0K5fQg8jjs6TXgJRyaP8Ulyzr1-NTw5X8",
+    apikey: key,
   },
 })
   .then((res) => res.json())
@@ -9,4 +13,5 @@ fetch("https://oyneqjqpxkustcsgrvut.supabase.co/rest/v1/vildmad", {
 
 function showData(items) {
   console.log(items);
+  items.For;
 }
